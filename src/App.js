e6057926c96a73fdb4522a7651de484add6774fb
newt0 <py_lambda@yahoo.co.jp>
2021-02-09 11:21:23 +0900

    add: render the data

--- src/App.js
+++ src/App.js
@@ -10,14 +10,26 @@ const App = () => {
       key: "1h3lSahG8BHHv7nhYFvctyv32Sf9MQBcCRCBk3gZdNOk",
       callback: (googleData) => {
         console.log("googleData>>>", googleData);
+        setData(googleData);
       },
       simpleSheet: true,
     });
   }, []);
 
+  console.log("updated state >>>", data);
+
   return (
     <div className="app">
       <h1>Google Sheets as CMS with React</h1>
+      <div id="trainer-details">
+        {data.map((obj) => (
+          <div key={obj.trainer}>
+            <p>{obj.trainer}</p>
+            <p>{obj.favDog}</p>
+            <img src={obj.img} alt={obj.trainer} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };

0530ca9bee73f340df9fc1cc3f873d9a0fe23577
newt0 <py_lambda@yahoo.co.jp>
2021-02-09 11:15:08 +0900

    add: fecthing data by Tabletop(BUT using callback prop)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import Tabletop from "tabletop";
 
 const App = () => {
-  return <div className="app"></div>;
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    Tabletop.init({
+      key: "1h3lSahG8BHHv7nhYFvctyv32Sf9MQBcCRCBk3gZdNOk",
+      callback: (googleData) => {
+        console.log("googleData>>>", googleData);
+      },
+      simpleSheet: true,
+    });
+  }, []);
+
+  return (
+    <div className="app">
+      <h1>Google Sheets as CMS with React</h1>
+    </div>
+  );
 };
 
 export default App;
